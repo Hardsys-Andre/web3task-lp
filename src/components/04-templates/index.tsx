4bd90485d1dd6ab0375921fdbd2635a9c941eaa2
Andre-Dev <hardsys_andre@hotmail.com>
2024-04-18 18:34:37 -0300

    feat/add FAQ Section

--- src/components/04-templates/index.tsx
+++ src/components/04-templates/index.tsx
@@ -3,5 +3,6 @@ export * from "./HeroSection";
 export * from "./HowItWorksSection";
 export * from "./PartnersSection";
 export * from "./RoadmapSection";
+export * from "./FaqSection";
 export * from "./WhyChooseUsSection";
 export * from "./TheFooterSection";
